ad56a026a8c1248b6db8b8c15367bcd57d31f708
lorehuincahue <lorehuincahue@gmail.com>
2022-08-29 00:54:14 +0000

    avance

--- src/js/views/personajes.js
+++ src/js/views/personajes.js
@@ -3,14 +3,12 @@ import React from "react";
 
 export const Card = (props) => {
     return (
-        <div className="card mx-2 mb-2" style={{ width: "18rem" }}>{/*<!--2 parentesis de llave  para mostrar q es JS y otro para demostrar que es un objeto-->*/}
+        <div className="card mx-2 mb-2" style={{ width: "18rem" }}>
             <img src={props.url} className="card-img-top mt-2 rounded" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}.</p>
-                <a href="#" className="btn btn-primary">
-                    Ver más sobre {props.title}
-                </a>
+                <a href="#" className="btn btn-primary"> {props.title} </a>
             </div>
         </div>
     );
